1c071f2705e54ef3360225280ee06fb4fea568f1
Aleksander Evensen <ahse03@gmail.com>
2025-10-17 18:03:13 +0200

    tsup build

---
+++ backend/tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  target: "node18",
+  outDir: "dist",
+  clean: true,
+  sourcemap: true,
+  minify: false,
+  splitting: false,
+  bundle: true,
+  platform: "node",
+});
