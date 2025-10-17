c1994bd5ec9aa7b9db85b2ea58635ac4d12154eb
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2025-10-17 17:13:09 +0200

    dockerized

---
+++ backend/deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+################################
+# CONFIGURATION
+# Change these values as needed
+DOMAIN="rema.tihlde.org"
+PORT=10000
+ENV_FILE_PATH=".env"
+################################
+
+COMMIT_HASH=$(git rev-parse --short HEAD)
+IMAGE_NAME="$DOMAIN:$COMMIT_HASH"
+
+echo "-> Building new Docker image"
+docker build -t $IMAGE_NAME .
+
+# REMOVE IF PROJECT DOES NOT USE PRISMA
+echo "-> Migrating database"
+prisma migrate deploy
+
+echo "-> Stopping and removing old container"
+docker rm -f $DOMAIN || true
+
+echo "-> Starting new container"
+docker run --env-file $ENV_FILE_PATH -p $PORT:3000 --name $DOMAIN --restart unless-stopped -d $IMAGE_NAME
